d0de53f0a0d71185bf42d364df1ecd82fbd817ed
Shachi-23 <shachilhgowda@gmail.com>
2025-03-11 16:08:11 +0530

    updated hello.cpp

--- main/hello.cpp
+++ main/hello.cpp
@@ -6,5 +6,7 @@ int main(void) {
   cout << "Hello, World\n";
   cout << "Hello, Jenkins\n";
   cout << "I have successfully built and run this  \n";
+  cout << "This is updated Code\n";
+  cout << "PES2UG22CS514\n";
   return 0;
 }
